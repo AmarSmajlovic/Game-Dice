9bbbeef851216952e5e455a2009ee7c9b00e57d0
AmarSmajlovic <amarcsgo@gmail.com>
2022-09-09 18:35:10 +0200

    eslint fix

--- src/modals/FinishGameModal/FinishGameModal.tsx
+++ src/modals/FinishGameModal/FinishGameModal.tsx
@@ -1,16 +1,18 @@
-import { useContext } from 'react'
-import { GameContextType, ModalContextType } from '../../@types'
-import { GameContext, ModalContext } from '../../contexts'
+import { useContext } from "react";
+import { GameContextType, ModalContextType } from "../../@types";
+import { GameContext, ModalContext } from "../../contexts";
+import "./styles.scss";
 
 const FinishGameModal = () => {
-  const { resetGame } = useContext(GameContext) as GameContextType
-  const { hideModal } = useContext(ModalContext) as ModalContextType
+  const { resetGame, rollNumber } = useContext(GameContext) as GameContextType;
+  const { hideModal } = useContext(ModalContext) as ModalContextType;
 
   return (
-    <div>
-      <button onClick={() => resetGame(hideModal)}>test</button>
+    <div className="container">
+      <h3>YOU FINISHED GAME WITH {rollNumber} ATTEMPT(S)</h3>
+      <button onClick={() => resetGame(hideModal)}>TRY AGAIN</button>
     </div>
-  )
-}
+  );
+};
 
-export default FinishGameModal
+export default FinishGameModal;
